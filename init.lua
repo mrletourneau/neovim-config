1b744ad61fa9c545de62529168dc85db10c51b95
Alexander Letourneau <letoura@amazon.com>
2024-02-08 10:26:53 -0500

    First commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- Lazy package manager config
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("alex-config")
+require("lazy").setup("plugins")
